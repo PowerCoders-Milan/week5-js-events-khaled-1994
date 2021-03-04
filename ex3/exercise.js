da68606e3de71fd2d1a66aec9919f3604085fe39
khaled sidawi <khaledassa123@gmail.com>
2021-03-04 17:37:19 +0100

    done

--- ex3/exercise.js
+++ ex3/exercise.js
@@ -8,14 +8,16 @@ but1.addEventListener('click', function mycounte(){
     counter ++;
     p.innerHTML = counter ;
     if(counter == 10){
-    image.innerHTML = "block"
+   
     image.setAttribute("src","https://tse2.mm.bing.net/th?id=OIP.h3Eqt3wBHuM0tMbslVUcdwHaEo&pid=Api&P=0&w=261&h=164");
     }
+
 })
 but2.addEventListener('click', function mycounte2(){
     counter --;
     p.innerHTML = counter ;
     if(counter == -10){
-        image.innerHTML = "block"
+        
     image.setAttribute("src","https://tse2.mm.bing.net/th?id=OIP.h3Eqt3wBHuM0tMbslVUcdwHaEo&pid=Api&P=0&w=261&h=164");
-    }})
+    }
+})
