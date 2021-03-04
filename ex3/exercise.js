584db2873df0366f6c74fd032cf4f84e82f35b01
khaled sidawi <khaledassa123@gmail.com>
2021-03-04 16:35:34 +0100

    not yet done

---
+++ ex3/exercise.js
@@ -0,0 +1,21 @@
+var p = document.querySelector('p');
+var but1 = document.getElementById('bt1');
+var but2 = document.getElementById('bt2');
+var image = document.createElement('img');
+document.body.appendChild(image);
+var counter = 0 ;
+but1.addEventListener('click', function mycounte(){
+    counter ++;
+    p.innerHTML = counter ;
+    if(counter == 10){
+    image.innerHTML = "block"
+    image.setAttribute("src","https://tse2.mm.bing.net/th?id=OIP.h3Eqt3wBHuM0tMbslVUcdwHaEo&pid=Api&P=0&w=261&h=164");
+    }
+})
+but2.addEventListener('click', function mycounte2(){
+    counter --;
+    p.innerHTML = counter ;
+    if(counter == -10){
+        image.innerHTML = "block"
+    image.setAttribute("src","https://tse2.mm.bing.net/th?id=OIP.h3Eqt3wBHuM0tMbslVUcdwHaEo&pid=Api&P=0&w=261&h=164");
+    }})
