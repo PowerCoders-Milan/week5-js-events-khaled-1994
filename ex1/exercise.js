825fb7881975e2a16775657450b0eaedaa332182
khaled sidawi <khaledassa123@gmail.com>
2021-03-04 12:06:37 +0100

    done

--- ex1/exercise.js
+++ ex1/exercise.js
@@ -1,3 +1,24 @@
 // define your functions here
-
 // call the functions and connect them to an event
+var as = document.querySelector('a');
+var st1 = document.getElementById('we');
+var st2 = document.getElementById('jav');
+var st3 = document.getElementById('pow');
+
+/*as.addEventListener('mousemove',function colo(){
+ st1.style.color = "red";
+    st2.style.color = "green";
+    st3.style.color = "yellow";
+    
+})*/
+//extra
+as.addEventListener('mousemove', function color1() {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    const randomColor1 = Math.floor(Math.random()*167227215).toString(10);
+    const randomColor2 = Math.floor(Math.random()*167237215).toString(11);
+    st1.style.backgroundColor = "#" + randomColor;
+    st2.style.backgroundColor = "#" + randomColor1;
+    st3.style.backgroundColor = "#" + randomColor2;
+
+    color.innerHTML = "#" + randomColor;
+  })
